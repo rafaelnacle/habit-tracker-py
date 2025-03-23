2ff2e6db6df244febd8be30ad22ce80e3433c7a4
= <=>
2025-03-23 20:39:58 -0300

    create the graph

--- main.py
+++ main.py
@@ -5,14 +5,38 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-pixela_endpoint = "https://pixe.la/v1/users"
+
+USERNAME = "rafaelnacle"
 TOKEN = os.getenv("TOKEN")
+
+pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
     "token": TOKEN,
-    "username": "rafaelnacle",
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-requests.post(url=pixela_endpoint, json=user_params)
+# Create the USER
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": "drawing",
+    "name": "Drawing Graph",
+    "unit": "times",
+    "type": "int",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# Create the graph
+# requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+# https://pixe.la/v1/users/rafaelnacle/graphs/drawing.html
+
